b1693228e9678b5529a5f333afa39ba861f387b8
TLaveau <tang.laveau@hotmail.fr>
2019-01-29 17:24:49 +0100

    migrate db

--- dawin_blog/db/migrate/20190129160017_create_comments.rb
+++ dawin_blog/db/migrate/20190129160017_create_comments.rb
@@ -1,6 +1,10 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.string :user_id
+      t.string :post_id
+      t.string :comment
+      t.timestamps :time
 
       t.timestamps null: false
     end
--- dawin_blog/db/migrate/20190129160334_create_posts.rb
+++ dawin_blog/db/migrate/20190129160334_create_posts.rb
@@ -1,6 +1,12 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.integer :user_id
+      t.string :title
+      t.string :image
+      t.timestamps :date
+      t.string :header
+
 
       t.timestamps null: false
     end
